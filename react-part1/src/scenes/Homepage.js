97e5d18ee4e3ec89a6d6191b87bee663ae2cac65
Rhama Fahri <rhamafahri18@gmail.com>
2023-03-19 07:12:13 +0700

    add parts Header (with import Navlink

--- react-part1/src/scenes/Homepage.js
+++ react-part1/src/scenes/Homepage.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 import Button from "element/Button";
+import Header from "parts/Header";
 
 export default function Homepage() {
   return (
     <div>
-      <Button isPrimary isSmall>
-        Sign Up
-      </Button>
+      <Header></Header>
     </div>
   );
 }
