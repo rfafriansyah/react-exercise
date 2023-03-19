129f1b2184630e9cead30387a1ad6ad8108e8ebc
Rhama Fahri <rhamafahri18@gmail.com>
2023-03-19 10:39:31 +0700

    update Section.js
    menambahkan animasi melalui react-reveal

--- react-part1/src/scenes/Homepage.js
+++ react-part1/src/scenes/Homepage.js
@@ -1,12 +1,45 @@
 import React from "react";
 
-import Button from "element/Button";
+import Fade from "react-reveal/Fade";
+import Section from "element/Section";
 import Header from "parts/Header";
-
+import HeroImage from "assets/images/hero-image.png";
 export default function Homepage() {
   return (
-    <div>
+    <div className="body-wrap">
       <Header></Header>
+      <main className="site-content">
+        <Section className="hero illustration-section-01" isCenteredContent>
+          <div className="container-sm">
+            <div className="hero-inner section-inner">
+              <div className="hero-content">
+                <Fade bottom delay={500}>
+                  <h1 className="mt-0 mb-16">Landing template for startup</h1>
+                </Fade>
+              </div>
+              <div className="container-xs">
+                <Fade bottom delay={1000}>
+                  <p className="mt-0 mb-32">
+                    Our landing page template works on all devices, so you only
+                    have to set it up once, and get beautiful result forever.
+                  </p>
+                </Fade>
+              </div>
+            </div>
+            <div className="hero-figure illustration-element-01">
+              <Fade bottom delay={1500}>
+                <img
+                  className="has-shadow"
+                  src={HeroImage}
+                  alt="Hero"
+                  width="896"
+                  height="504"
+                />
+              </Fade>
+            </div>
+          </div>
+        </Section>
+      </main>
     </div>
   );
 }
