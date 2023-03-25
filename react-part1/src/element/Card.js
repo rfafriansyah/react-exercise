cb0f964ab7291614c6adf438089d9c623441aa2e
Rhama Fahri <rhamafahri18@gmail.com>
2023-03-25 11:04:50 +0700

    add features and add card (progress)

---
+++ react-part1/src/element/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import propTypes from "prop-types";
+
+export default function Card(props) {
+  return <div>Card</div>;
+}
+
+Card.propTypes = {};
--- react-part1/src/scenes/Homepage.js
+++ react-part1/src/scenes/Homepage.js
@@ -1,10 +1,39 @@
 import React from "react";
-
 import Fade from "react-reveal/Fade";
+
 import Section from "element/Section";
 import Header from "parts/Header";
+import Clients from "parts/Clients";
+import Feature from "parts/Feature";
+
 import HeroImage from "assets/images/hero-image.png";
+import FeatureTile01 from "assets/images/feature-tile-icon-01.svg";
+import FeatureTile02 from "assets/images/feature-tile-icon-02.svg";
+import FeatureTile03 from "assets/images/feature-tile-icon-03.svg";
 export default function Homepage() {
+  const features = [
+    {
+      imgSrc: FeatureTile01,
+      imgAlt: "Feature tile icon 01",
+      title: "Join Rhama 01",
+      description:
+        "A pseudo-Latin text used in web design, layout, and printing in place of thigns to emphasise design.",
+    },
+    {
+      imgSrc: FeatureTile02,
+      imgAlt: "Feature tile icon 02",
+      title: "Join Rhama 02",
+      description:
+        "A pseudo-Latin text used in web design, layout, and printing in place of thigns to emphasise design.",
+    },
+    {
+      imgSrc: FeatureTile03,
+      imgAlt: "Feature tile icon 03",
+      title: "Join Rhama 03",
+      description:
+        "A pseudo-Latin text used in web design, layout, and printing in place of thigns to emphasise design.",
+    },
+  ];
   return (
     <div className="body-wrap">
       <Header></Header>
@@ -36,6 +65,38 @@ export default function Homepage() {
                   height="504"
                 />
               </Fade>
+            </div>
+          </div>
+        </Section>
+        <Clients></Clients>
+        <Section className="features-tiles">
+          <div className="container">
+            <div className="features tiles inner section-inner">
+              <div className="tiles-wrap">
+                {features.map((feature, index) => (
+                  <Feature
+                    key={index}
+                    delayInMS={index * 500}
+                    data={feature}
+                  ></Feature>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Section>
+        <Section className="pricing">
+          <div className="container">
+            <div className="pricing-inner section-inner has-top-divider">
+              <div className="section-header center-content">
+                <div className="container-xs">
+                  <h2 className="mt-0 mb-16">Simple, transparent pricing</h2>
+                  <p className="m-0">
+                    Lorem ipsum common placeholder text text used to demonstrate
+                    the grapic elements of a documents or visual presentation.
+                  </p>
+                </div>
+              </div>
+              <div className="tiles-wrap"></div>
             </div>
           </div>
         </Section>
